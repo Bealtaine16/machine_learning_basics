909b480100095496b628810c40f9505a37b3cc7b
Maja Rusak <maja.rusak16@gmail.com>
2023-08-27 11:51:46 +0200

    Gradient descent

--- gradient/gradient.py
+++ gradient/gradient.py
@@ -3,47 +3,51 @@ import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Constants
 alfa_list = [1, 10, 100]
 n_list = [10, 20]
-learning_rate = 0.001
-precision = 1e-05
+precision = 0.0001  # 1e-05
+learning_rate = 0.1  # 0.01
+epoch_limit = 1000
 
 
 def output(x, alfa, n):
-    y = sum((alfa ** ((i - 1) / (n - 1))) * (x[i - 1] ** 2) for i in range(1, n + 1))
+    i = np.arange(1, n + 1)
+    y = np.sum((alfa ** ((i - 1) / (n - 1))) * (x**2))
     return y
 
 
-def output_predicted(x, theta, n):
-    y = sum(theta[i - 1] * (x[i - 1] ** 2) for i in range(1, n + 1))
-    return y
-
-
-def derivative_mse(x, y_act, n, theta):
-    dy = np.zeros(n)
-    for i in range(n):
-        dy[i] = 2 * (x[i] ** 2) * (theta[i] * (x[i] ** 2) - y_act)
+def derivative(x, alfa, n):
+    i = np.arange(1, n + 1)
+    dy = (2 * alfa ** ((i - 1) / (n - 1))) * x
     return dy
+
+
+def derivative_second(x, alfa, n):
+    i = np.arange(1, n + 1)
+    d2y = 2 * alfa ** ((i - 1) / (n - 1))
+    return d2y
 
 
 def gradient_descent_algorithm(alfa, n, epoch_size):
     random.seed(n)
-    theta = np.array([random.uniform(-1, 1) for i in range(n)])
-    x_inputs = np.array([random.uniform(-10, 10) for i in range(n)])
-    y_act = output(x_inputs, alfa, n)
+    # theta = np.array([random.uniform(-100, 100) for i in range(n)])
+    theta = np.array([76, -5, 96, 18, -43, -63, 54, 71, 48, 7])
     cost_list = []
     mse_prev = None
-    print(x_inputs, y_act, theta)
 
     for epoch in range(epoch_size):
-        gradient = derivative_mse(x_inputs, y_act, n, theta)
+        y = output(theta, alfa, n)
+        gradient = derivative(theta, alfa, n)
         theta = theta - learning_rate * gradient
-        mse = np.sum((output_predicted(x_inputs, theta, n) - y_act) ** 2)
+
+        y_new = output(theta, alfa, n)
+        mse = np.sum((y_new - y) ** 2)
         if mse_prev and abs(mse_prev - mse) <= precision:
             break
         mse_prev = mse
-        print(y_act, output_predicted(x_inputs, theta, n), gradient)
         cost_list.append((epoch, mse))
+        print(epoch, mse)
 
     return cost_list
 
@@ -51,7 +55,7 @@ def gradient_descent_algorithm(alfa, n, epoch_size):
 def main():
     start_dt = dt.datetime.now()
 
-    cost = gradient_descent_algorithm(alfa_list[0], n_list[0], epoch_size=1000)
+    cost = gradient_descent_algorithm(alfa_list[0], n_list[0], epoch_size=epoch_limit)
 
     end_dt = dt.datetime.now()
     print("Time taken: %s" % (end_dt - start_dt))
@@ -59,30 +63,11 @@ def main():
     x_epoch, y_cost = zip(*cost)
 
     plt.plot(x_epoch, y_cost, ".-", color="red")
+    plt.xlabel("Epoch")
+    plt.ylabel("Mean Squared Error")
+    plt.title("Gradient Descent Convergence")
     plt.show()
 
 
 if __name__ == "__main__":
     main()
-
-    # for epoch in range(epoch_size):
-
-    #     act = [y for y in inputs(x_inputs, alfa, n)]
-    #     print(act)
-    #     grad = [gradient_descent(x, y, theta) for x, y in inputs(x_inputs, alfa, n)]
-    #     mse = np.square(np.subtract(act, grad)).mean()
-    #     grad_mean = np.mean(grad)
-    #     theta = gradient_step(theta, grad, -learning_rate)
-    #     if np.all(theta == [0, 0]):
-    #         print(epoch)
-    #         break
-    #     cost_list.append((epoch, mse))
-
-    # return cost_list
-
-    #     y_pred = output(x_inputs, alfa, n)
-    # mse = np.sum((y_act - y_pred) ** 2)
-    # if mse and abs(mse_prev - mse) <= precision:
-    #     break
-    # mse_prev = mse
-    # cost_list.append((epoch, mse))
